2c1f1969ccd487e6a65f604e9117afec8d787f64
aqsa-eng <nmalbaig@gmail.com>
2026-05-22 14:44:16 -0700

    fixing

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import { ConnectMongodb } from "./Utils/mongodb.js";
-import URLRoute from "./Routes/urls.js"
+import URLRoute from "./Routes/urls.js";
 import { connectRedis } from "./Utils/redis.js";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
-app.options("*", cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    if (req.method === "OPTIONS") return res.sendStatus(200);
+    next();
+});
 
 app.use(express.json());
 
