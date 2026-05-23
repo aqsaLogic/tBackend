395f5d8bf931258ea48ac0a380802da85fbcd28c
aqsa-eng <nmalbaig@gmail.com>
2026-05-23 13:37:00 -0700

    push

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import { ConnectMongodb } from "./Utils/mongodb.js";
 import URLRoute from "./Routes/urls.js";
-import { connectRedis } from "./Utils/redis.js";
 
 dotenv.config();
 
 const app = express();
 
-// connectRedis();
-app.use(cors());
+// CORS manually — koi library nahi
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+    next();
+});
+
 app.use(express.json());
 
 ConnectMongodb();
